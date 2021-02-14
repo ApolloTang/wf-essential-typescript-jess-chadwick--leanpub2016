85877bf1f191e7cefa066b70757f6d795ff09568
Apollo Tang <tang.apollo@gmail.com>
2021-02-14 17:00:12 -0500

    add what namespace is not supported by babel

---
+++ chap09-modules/organizing-your-code-w-namespaces/babel-w-namespace/0-this-works/transpiled-w-tsc.js
@@ -0,0 +1,10 @@
+var Model;
+(function (Model) {
+    var TodoState;
+    (function (TodoState) {
+        TodoState[TodoState["New"] = 1] = "New";
+        TodoState[TodoState["Active"] = 2] = "Active";
+        TodoState[TodoState["Complete"] = 3] = "Complete";
+        TodoState[TodoState["Deleted"] = 4] = "Deleted";
+    })(TodoState = Model.TodoState || (Model.TodoState = {}));
+})(Model || (Model = {}));
